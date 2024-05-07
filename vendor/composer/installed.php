30b0ba4bd2a4c1baa6b7548ebe6bb5680151a887
saul <saul.miquilena@gmail.com>
2024-05-07 18:06:44 -0400

    Inicio del Proyecto propertyweb. Carga de Bootstrap 5.3.3 y creacion del index.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6859298e8ddd15f21db76c6c37c571dba3075261',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6859298e8ddd15f21db76c6c37c571dba3075261',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v5.3.3',
+      'version' => '5.3.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6e1f75f420f68e1d52733b8e407fc7c3766c9dba',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v5.3.3',
+      ),
+    ),
+  ),
+);
